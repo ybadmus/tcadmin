efe8c482497545d1ac91570b383ddd0db3e03944
Anthony Mensah <Mensah@persolgh.com>
2020-02-10 09:10:47 +0000

    devices view setup

---
+++ TaxCollectorAdmin/Controllers/DeviceController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TaxCollectorAdmin.Controllers
+{
+    public class DeviceController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Devices()
+        {
+            return View();
+        }
+
+        public IActionResult CreateDevice()
+        {
+            return View();
+        }
+    }
+}
